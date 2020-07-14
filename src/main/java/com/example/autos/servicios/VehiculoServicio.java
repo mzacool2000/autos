61c74b2536bf35e024c34edac6255befe051a6e6
franciscozacca <francisco.zacca1965@gmail.com>
2020-07-14 20:14:34 -0300

    termine mi parte en  mi rama

--- src/main/java/com/example/autos/servicios/VehiculoServicio.java
+++ src/main/java/com/example/autos/servicios/VehiculoServicio.java
@@ -1,7 +1,10 @@
 package com.example.autos.servicios;
 
+import com.example.autos.entidades.Foto;
+import com.example.autos.entidades.Marca;
 import com.example.autos.entidades.Vehiculo;
 import com.example.autos.enums.TipoCombustible;
+import com.example.autos.repositorios.MarcaRepositorio;
 import com.example.autos.repositorios.VehiculoRepositorio;
 import java.util.Optional;
 import javax.transaction.Transactional;
@@ -14,18 +17,23 @@ public class VehiculoServicio {
 
     @Autowired
     private VehiculoRepositorio vehiculoRepositorio;
-    
-    
+
+    @Autowired
+    private MarcaRepositorio marcaRepositorio;
+
+    @Autowired
+    private FotoServicio fotoServicio;
 
     //AGREGAR -usuarioRepositorio-
-    //AGREGAR -fotoServicio-
     @Transactional
-    public void AgregarVehiculo( MultipartFile archivo ,String id, Marca marca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto, boolean habilitado) throws Error {
+    public void AgregarVehiculo(MultipartFile archivo, String idMarca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto, boolean habilitado) throws Error {
 
         //AGREGAR USUARIOREPOSITORIO
-        validar(modelo, marca);
+        validar(modelo, idMarca, motor, tipo, cilindrada, emision, consumoRuta, consumoUrbano, consumoMixto);
+        Optional<Marca> respuestaM = marcaRepositorio.findById(idMarca);
+
         Vehiculo vehiculo = new Vehiculo();
-        vehiculo.setMarca(marca);
+        vehiculo.setMarca(respuestaM.get());
         vehiculo.setModelo(modelo);
         vehiculo.setMotor(motor);
         vehiculo.setTipo(tipo);
@@ -36,21 +44,91 @@ public class VehiculoServicio {
         vehiculo.setConsumoMixto(consumoMixto);
         vehiculo.setHabilitado(habilitado);
 
-//        Foto foto = fotoServicio.guardar(archivo);
+        Foto foto = fotoServicio.guardar(archivo);
         vehiculo.setFoto(foto);
         vehiculoRepositorio.save(vehiculo);
     }
-    public void modificar(MultipartFile archivo ,String id, Marca marca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto, boolean habilitado )throws Error{
-         validar(modelo, marca);
-         
-         Optional<Vehiculo> respuesta = vehiculoRepositorio.findById(id);
-         if (respuesta.isPresent()) {
-            Vehiculo vehiculo = respuesta.get();
-             if (vehiculo.) {
-                 
-             }
+
+    @Transactional
+    public void modificar(MultipartFile archivo, String id, String idMarca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto) throws Error {
+        if (id.isEmpty() || id == null) {
+           throw new Error("El Id del vehiculo no puede ser nulo."); 
         }
-         
+        validar(modelo, idMarca, motor, tipo, cilindrada, emision, consumoRuta, consumoUrbano, consumoMixto);
+
+        Optional<Vehiculo> respuestaV = vehiculoRepositorio.findById(id);
+
+        Optional<Marca> respuestaM = marcaRepositorio.findById(idMarca);
+        if (respuestaV.isPresent()) {
+            Vehiculo vehiculo = respuestaV.get();
+            if (respuestaM.isPresent()) {
+                if (archivo != null) {
+                    vehiculo.setFoto(fotoServicio.guardar(archivo));
+                }
+                vehiculo.setMarca(respuestaM.get());
+                vehiculo.setCilindrada(cilindrada);
+                vehiculo.setConsumoMixto(consumoMixto);
+                vehiculo.setConsumoRuta(consumoRuta);
+                vehiculo.setConsumoUrbano(consumoUrbano);
+                vehiculo.setEmision(emision);
+                vehiculo.setId(id);
+                vehiculo.setModelo(modelo);
+                vehiculo.setMotor(motor);
+                vehiculo.setTipo(tipo);
+                vehiculoRepositorio.save(vehiculo);
+            } else {
+                throw new Error("No se ha encotrado la marca");
+            }
+        } else {
+            throw new Error("No se encontro el vehiculo");
+        }
+
     }
-    
+
+    public void eliminar(String id) throws Error {
+
+        Optional<Vehiculo> respuestaV = vehiculoRepositorio.findById(id);
+
+        if (respuestaV.isPresent()) {
+            Vehiculo vehiculo = respuestaV.get();
+            vehiculo.setHabilitado(false);
+            vehiculoRepositorio.save(vehiculo);
+
+        } else {
+            throw new Error("No existe un ningun vehiculo con ese indicador");
+        }
+    }
+
+    public void validar(String idMarca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto) throws Error {
+        
+        if (modelo == null || modelo.isEmpty()) {
+            throw new Error("El nombre del modelo no puede ser vacio");
+        }
+        if (idMarca == null || idMarca.isEmpty()) {
+            throw new Error("El id de la marca no puede ser vacio");
+        }
+        if (motor == null || motor.isEmpty()) {
+            throw new Error("El motor no puede ser vacio");
+        }
+        if (tipo == null) {
+            throw new Error("El tipo de combustible no puede ser vacio");
+        }
+        if (cilindrada == null) {
+            throw new Error("La cilindrada no puede ser vacio");
+        }
+        if (emision == null) {
+            throw new Error("La emision no puede ser vacio");
+        }
+        if (consumoRuta == null) {
+            throw new Error("El consumo de ruta no puede ser vacio");
+        }
+        if (consumoUrbano == null) {
+            throw new Error("El consumo urbano no puede ser vacio");
+        }
+        if (consumoMixto == null) {
+            throw new Error("El consumo mixto no puede ser vacio");
+        }
+
+    }
+
 }
