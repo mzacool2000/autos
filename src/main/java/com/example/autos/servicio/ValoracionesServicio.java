4b74c19f5b02bad95a39ca92eddb6c51bb78056a
mzacool2000 <chirino@outlook.com>
2020-07-16 18:15:48 -0300

    mas arreglos

--- src/main/java/com/example/autos/servicio/ValoracionesServicio.java
+++ src/main/java/com/example/autos/servicio/ValoracionesServicio.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.example.autos.repositorio;
+package com.example.autos.servicio;
 
-import com.example.autos.entidades.Usuario;
+
 import com.example.autos.entidades.Valoraciones;
 import org.springframework.data.jpa.repository.JpaRepository;
 
