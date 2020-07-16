c1dd743d5cdaca6f4f880b1d3311fe5d0767ab66
mzacool2000 <chirino@outlook.com>
2020-07-16 13:54:55 -0300

    comparaciones

---
+++ src/main/java/com/example/autos/repositorio/VehiculoRepositorio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.autos.repositorio;
+
+import com.example.autos.entidades.Vehiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author chiri
+ */
+public interface VehiculoRepositorio  extends JpaRepository<Vehiculo, String> {
+    
+}
