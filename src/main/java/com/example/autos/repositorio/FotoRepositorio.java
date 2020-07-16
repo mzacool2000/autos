6cdbd02074827aa88422175574f4673c81f55520
Andresarrollador <67170636+Andresarrollador@users.noreply.github.com>
2020-07-16 18:02:50 -0300

    Arreglo definitivo
    arreglo definitivo

--- src/main/java/com/example/autos/repositorio/FotoRepositorio.java
+++ src/main/java/com/example/autos/repositorio/FotoRepositorio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.example.autos.repositorios;
+package com.example.autos.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
