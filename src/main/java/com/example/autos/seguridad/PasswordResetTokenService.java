4f42f204d6ab4dd97dbbf982701b0780dd3f495b
mzacool2000 <chirino@outlook.com>
2020-07-16 09:29:23 -0300

    token seguuirdad
    agrege los tokens de seguirad

---
+++ src/main/java/com/example/autos/seguridad/PasswordResetTokenService.java
@@ -0,0 +1,23 @@
+
+
+
+package com.example.autos.seguridad;
+
+import com.example.autos.repositorio.PasswordResetTokenRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+
+@Service
+public class PasswordResetTokenService {
+    
+@Autowired
+private PasswordResetTokenRepositorio passwordResetTokenRepositorio;
+
+@Autowired
+        
+    
+    
+}
