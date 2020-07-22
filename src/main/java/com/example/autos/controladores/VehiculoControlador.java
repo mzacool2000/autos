65382129d8e1b699a88eea89e6a9f767356194ae
mzacool2000 <chirino@outlook.com>
2020-07-22 08:26:10 -0300

    arreglo del index y gestor de vehiculos

--- src/main/java/com/example/autos/controladores/VehiculoControlador.java
+++ src/main/java/com/example/autos/controladores/VehiculoControlador.java
@@ -3,6 +3,7 @@ package com.example.autos.controladores;
 
 import com.example.autos.enums.TipoCombustible;
 import com.example.autos.repositorio.MarcaRepositorio;
+import com.example.autos.repositorio.VehiculoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,10 +15,17 @@ public class VehiculoControlador {
 
 @Autowired
 private MarcaRepositorio marcaRepositorio;
+@Autowired
+private VehiculoRepositorio vehiculoRepositorio;
 
     
 @GetMapping("/agregarvehiculo")
 public String agregavehiculo(ModelMap modelo){
+    
+   
+    
+    
+    modelo.put("vehiculos",vehiculoRepositorio.findAll());
 
     modelo.put("marcas", marcaRepositorio.findAll());
     
