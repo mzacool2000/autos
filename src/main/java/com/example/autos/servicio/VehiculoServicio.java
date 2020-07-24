bec16f504eab6477468fe8f591486eac8d6f5180
mzacool2000 <chirino@outlook.com>
2020-07-24 18:12:18 -0300

    actualizador

--- src/main/java/com/example/autos/servicio/VehiculoServicio.java
+++ src/main/java/com/example/autos/servicio/VehiculoServicio.java
@@ -55,7 +55,7 @@ public class VehiculoServicio {
     }
 
     @Transactional
-    public void modificar(MultipartFile archivo, String id, String idMarca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto) throws Error {
+    public void modificar(MultipartFile archivo, String id, String idMarca, String modelo, String motor, TipoCombustible tipo, Integer cilindrada, Double emision, Double consumoRuta, Double consumoUrbano, Double consumoMixto, boolean habilitado) throws Error {
         if (id.isEmpty() || id == null) {
            throw new Error("El Id del vehiculo no puede ser nulo."); 
         }
@@ -80,6 +80,7 @@ public class VehiculoServicio {
                 vehiculo.setModelo(modelo);
                 vehiculo.setMotor(motor);
                 vehiculo.setTipo(tipo);
+                vehiculo.setHabilitado(habilitado);
                 vehiculoRepositorio.save(vehiculo);
             } else {
                 throw new Error("No se ha encotrado la marca");
