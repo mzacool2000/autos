21581d7d88bab841406c56129b4c6297580e8425
mzacool2000 <chirino@outlook.com>
2020-07-24 21:28:34 -0300

    Revert "actualizador"
    This reverts commit bec16f504eab6477468fe8f591486eac8d6f5180.

--- src/main/java/com/example/autos/controladores/VehiculoControlador.java
+++ src/main/java/com/example/autos/controladores/VehiculoControlador.java
@@ -2,19 +2,14 @@
 package com.example.autos.controladores;
 
 import com.example.autos.entidades.Vehiculo;
-import com.example.autos.enums.TipoCombustible;
 import com.example.autos.repositorio.MarcaRepositorio;
 import com.example.autos.repositorio.VehiculoRepositorio;
-import com.example.autos.servicio.VehiculoServicio;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class VehiculoControlador {
@@ -23,11 +18,12 @@ public class VehiculoControlador {
 private MarcaRepositorio marcaRepositorio;
 @Autowired
 private VehiculoRepositorio vehiculoRepositorio;
-@Autowired
-private VehiculoServicio vehiculoServicio;
+
     
 @GetMapping("/agregarvehiculo")
 public String agregavehiculo(ModelMap modelo){
+    
+   
     
     
     modelo.put("vehiculos",vehiculoRepositorio.findAll());
@@ -50,45 +46,5 @@ public String editarv(ModelMap modelo , @PathVariable String id){
     }
 
 return "/agregarvehiculo";
-}
-
-@PostMapping("/adminvehiculo")
-private String adminvehiculo(ModelMap model, @RequestParam(required = false) String id, @RequestParam String modelo,@RequestParam String marcaid,
-       @RequestParam String motor, @RequestParam int cilindrada, @RequestParam double emision, @RequestParam double consumoRuta, @RequestParam double consumoCiudad,
-       @RequestParam double consumoMixto, @RequestParam TipoCombustible combustible, @RequestParam(required = false) String habilitado, @RequestParam(required = false) MultipartFile archivo ){
-       
-       
-    try {
-            boolean chk = !(habilitado == null);
-            
-            if (id.isEmpty() || id == null) {
-                vehiculoServicio.AgregarVehiculo(archivo, marcaid, modelo, motor, combustible, cilindrada, emision, consumoRuta, consumoCiudad, consumoMixto, chk);
-            }else{
-            
-            vehiculoServicio.modificar(archivo, id, marcaid, modelo, motor, combustible, cilindrada, emision, consumoRuta, consumoCiudad, consumoMixto,chk);
-            return "redirect:/agregarvehiculo";
-            }
-    } catch (Exception e) {
-        model.put("error", e.getMessage());
-        return "redirect:/agregarvehiculo";
-    }
-    return "redirect:/agregarvehiculo";
-}
-@PostMapping("/autodelet")
-public String autodelet(@RequestParam String id, ModelMap modelo){
-
-    try {
-        vehiculoServicio.eliminar(id);
-        modelo.put("titulo", "Eliminado");
-        modelo.put("mensaje", "El vehiculo fue eliminado con extio");
-        
-    } catch (Exception e) {
-         vehiculoServicio.eliminar(id);
-        modelo.put("error", "oke");
-        modelo.put("mensaje", "El vehiculo fue eliminado con extio");
-        return "exito,html";
-    }
-    
-return "exito.html";    
 }
 }
